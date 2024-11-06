6817bf09c869282615432db675a42da1f9a00632
Alexej Yarotski <alex.yarotski@gmail.com>
2024-11-06 15:36:04 +0300

    Added and connected Final script

--- Assets/Project/Dev/Scripts/Command/CheckScoreCommand.cs
+++ Assets/Project/Dev/Scripts/Command/CheckScoreCommand.cs
@@ -1,5 +1,4 @@
 using Naninovel;
-using UnityEngine;
 
 [CommandAlias("checkScore")]
 public class CheckScoreCommand : Command
@@ -7,7 +6,9 @@ public class CheckScoreCommand : Command
     public override UniTask ExecuteAsync (AsyncToken asyncToken = default)
     {
         var score = ScoreManager.Instance.GetScore();
-        Debug.Log($"Current Score: {score}");
+        
+        Engine.GetService<ICustomVariableManager>().TrySetVariableValue("PlayerScore", score);
+
         return UniTask.CompletedTask;
     }
 }
